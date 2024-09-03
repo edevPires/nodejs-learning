b4fe18079bdc3c85d891452e1838670ffe8d6ffc
edevPires <133178329+edevPires@users.noreply.github.com>
2024-09-03 08:35:37 -0300

    Day 2

---
+++ 5-Parsing-Request-Bodies/app.js
@@ -0,0 +1,43 @@
+const http = require("http")
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    const url = req.url
+    const method = req.method
+
+    res.setHeader('Content-Type', 'text/html')
+
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>Response</title></head>')
+        res.write("<form action='/message' method='POST'>")
+        res.write("<input name='message' type='text'>")
+        res.write("<input type='submit'>")
+        res.write("</form>")
+        res.write('</html>')
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk)
+        })
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=',)[1]
+            // Nem eu entendi essa parada ai do replace     vvvv
+            fs.writeFileSync('message.txt', message.replace(/\+/g, ' '))
+        })
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+    }
+    res.write('<html>')
+    res.write('<head><title>Response</title></head>')
+    res.write("<h1>Hello, i'm a response :D")
+    res.write('</html>')
+    res.end()
+})
+
+server.listen(3000)
